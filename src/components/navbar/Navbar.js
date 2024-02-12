272ebaa013d39f66e2c992eefc2450c04cde4781
Gustavo Meneses <gdameneses@gmail.com>
2024-02-12 15:49:14 -0300

    feat: added smooth transition to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,29 @@
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './Navbar.scss'
+import './Navbar.scss';
 
 function NavBar() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <Navbar expand="lg" className="navbar">
-      <Container fluid="md" className="navbar__container p-0">
+      <div id="background" className={`${scrollPosition > 16 ? 'navbar__background--active' : 'navbar__background'}`}></div>
+      <Container fluid="md" className="navbar__container">
         <Navbar.Brand href="#home" className="navbar__brand">Gustavo Meneses</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="navbar__toggle" />
         <Navbar.Collapse id="basic-navbar-nav" className="navbar__links">
