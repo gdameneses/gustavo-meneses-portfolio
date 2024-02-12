aa9782bb19f147b4bdc90b482e2db66714578f43
Gustavo Meneses <gdameneses@gmail.com>
2024-02-12 20:54:23 -0300

    chore: change vertical size of hero and cards

--- src/components/cardDivisor/CardDivisor.js
+++ src/components/cardDivisor/CardDivisor.js
@@ -1,20 +1,25 @@
 import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import './CardDivisor.scss'
 
 function CardDivisor() {
     const card = ({title, text}) => {
         return (
+            <Col xs={8} md={3}>
             <Card className="divisor__card">
                 <Card.Body>
                     <Card.Title>{title}</Card.Title>
                     <Card.Text>{text}</Card.Text>
                 </Card.Body>
             </Card>
+            </Col>
         )
     }
     return (
-      <Container className="d-flex flex-row justify-content-around">
+      <Container>
+        <Row className="cards__wrapper">
         {card({
             title: `Card Title`,
             text:
@@ -38,8 +43,16 @@ function CardDivisor() {
               on the card title and make up
               the bulk of the card content.`
             }
+        )}  {card({
+            title: `Card Title`,
+            text:
+              `Some quick example text to build
+              on the card title and make up
+              the bulk of the card content.`
+            }
         )}
-        </Container>
+        </Row>
+      </Container>
     );
 }
 
