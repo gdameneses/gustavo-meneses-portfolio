81935088fc3f8b1223e45277bebb9f82d1f855c1
Gustavo Meneses <gdameneses@gmail.com>
2024-02-12 17:11:26 -0300

    chore: header responsiveness

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col'
 import './Header.scss';
 
+const BlurredElipse = ({top, left, width, height, color1, color2, color3}) => {
+    const [angle, setAngle] = useState(45); // Initial angle value
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setAngle(angle => (angle + 1) % 361);
+        }, 4);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+<div style={{top, left, height, width, '--angle': `${angle}deg`, '--color1': `${color1}`, '--color2': `${color2}`, '--color3': `${color3}`}} className="background__item "></div>
+    );
+};
+
+
+
+
+
 const Header = () => {
   return (
-    <Container fluid className= "hero p-0">
-      <Container fluid="md" className="hero__content p-0">
-        <div className="hero__subtitle">
+    <Container fluid className="hero">
+    <div className="background">
+      <div className="background__img">
+        {BlurredElipse({top: '30%', left: '10%', width: '30%', height: '40%', color1: '#0029FF', color2: '#00FFB2', color3: '#FF00E5'})}
+        {BlurredElipse({top: '10%', left: '40%', width: '20%', height: '30%', color1: '#0029FF', color2: '#FF00D6', color3: '#00B2FF'})}
+        {BlurredElipse({top: '50%', left: '60%', width: '25%', height: '30%', color1: '#0029FF', color2: '#FF00D6', color3: '#00B2FF'})}
+        {BlurredElipse({top: '12%', left: '80%', width: '24%', height: '40%', color1: '#FF0000', color2: '#CCFF00', color3: '#00FF94'})}
+      </div>
+    </div>
+      <Container fluid="md" className="hero__content">
+        <div className="hero__subtitle align-items-start">
           <div className="hero__subtitle__block--left">
             <p className="hero__text__content">Hi, I am Gus!</p>
             <p className="hero__text__content">Welcome to my portfolio</p>
@@ -23,7 +51,7 @@ const Header = () => {
             <h1 className="hero__header__text--right">DEVELOPER</h1> 
           </div>
           <div className="d-flex justify-content-between flex-column flex-md-row">
-            <Col xs={12} md={4} className="hero_footer">
+            <Col xs={12} md={5} className="hero_footer">
               <p>
               A developer is not merely a coder they are architects 
               of innovation. Problem solvers, and pioneers of progress in the digital age. As technology continues to advance, the role of developers remains pivotal.
